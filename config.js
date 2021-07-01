1b695e28e0982c9e5da6541200400f23b9c34fd5
hoangphuc18012005 <86758558+hoangphuc18012005@users.noreply.github.com>
2021-07-01 10:52:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay khi anh xong dl anh sẽ nhắn cho em nha, đừng làm bản thân mệt quá đó nha bae.',
+    messDesc: 'Tối nay em nhớ cố ngủ sớm nha bae.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
